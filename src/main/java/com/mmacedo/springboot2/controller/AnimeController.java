170c7c56d2944c4f7f0c3931ef087664840777e8
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-24 11:13:17 -0300

    hot swap with spring boot devtools

--- src/main/java/com/mmacedo/springboot2/controller/AnimeController.java
+++ src/main/java/com/mmacedo/springboot2/controller/AnimeController.java
@@ -24,6 +24,6 @@ public class AnimeController {
     @GetMapping(path = "list")
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return List.of(new Anime("Boku no hero"), new Anime("Berserk"));
     }
 }
