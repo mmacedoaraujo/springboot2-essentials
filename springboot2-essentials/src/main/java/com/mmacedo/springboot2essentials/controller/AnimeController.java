d6d72865c85f80cec9b16aa03f2f68f91547a25a
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-26 20:31:30 -0300

    get method pt 2

--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/controller/AnimeController.java
@@ -6,7 +6,10 @@ import com.mmacedo.springboot2essentials.service.AnimeService;
 import com.mmacedo.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +24,13 @@ public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
-    @GetMapping(path = "list")
-    public List<Anime> list() {
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return AnimeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return new  ResponseEntity<>(animeService.findById(id), HttpStatus.OK);
     }
 }
--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/service/AnimeService.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/service/AnimeService.java
@@ -1,14 +1,24 @@
 package com.mmacedo.springboot2essentials.service;
 
 import com.mmacedo.springboot2essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L, "Boku no hero"), new Anime(2L,"Berserk"));
 
-    public static List<Anime> listAll() {
-        return List.of(new Anime(1L, "Boku no hero"), new Anime(2L,"Berserk"));
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
