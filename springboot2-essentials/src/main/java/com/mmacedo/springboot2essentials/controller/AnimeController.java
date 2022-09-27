5863a5ff435cc52a6ef670e732f7963f22b574c5
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-26 21:04:27 -0300

    post method

--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/controller/AnimeController.java
@@ -6,12 +6,10 @@ import com.mmacedo.springboot2essentials.service.AnimeService;
 import com.mmacedo.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,5 +30,10 @@ public class AnimeController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable Long id) {
         return new  ResponseEntity<>(animeService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
