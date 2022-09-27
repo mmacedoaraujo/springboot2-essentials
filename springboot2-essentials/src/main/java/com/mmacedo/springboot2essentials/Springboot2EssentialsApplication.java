60e7c47bc2d0185ff29ee2e19e12c58067e0f8ab
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-27 12:33:38 -0300

    spring data jpa pt01

--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/Springboot2EssentialsApplication.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/Springboot2EssentialsApplication.java
@@ -2,8 +2,9 @@ package com.mmacedo.springboot2essentials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Springboot2EssentialsApplication {
 
 	public static void main(String[] args) {
--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/domain/Anime.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/domain/Anime.java
@@ -1,11 +1,21 @@
 package com.mmacedo.springboot2essentials.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
