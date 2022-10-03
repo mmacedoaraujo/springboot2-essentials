05717f164d8869947b067342823fc0e1d454fe8d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-03 20:44:34 -0300

    WebMvcConfigurer

---
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/configurer/DevDojoWebMvcConfigurer.java
@@ -0,0 +1,17 @@
+package com.mmacedo.springboot2essentials.configurer;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+public class DevDojoWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(pageHandler);
+    }
+}
