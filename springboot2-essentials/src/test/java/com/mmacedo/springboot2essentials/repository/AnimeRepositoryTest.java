3fd6ab7e52370089834733a3d678683bc0765322
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-06 20:18:22 -0300

    spring data jpa test pt01

---
+++ springboot2-essentials/src/test/java/com/mmacedo/springboot2essentials/repository/AnimeRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.mmacedo.springboot2essentials.repository;
+
+import com.mmacedo.springboot2essentials.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@DisplayName("Tests for anime repository")
+class AnimeRepositoryTest {
+
+    //in test cases is okay to use @Autowired
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save creates anime when succesful")
+    void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime savedAnime = this.animeRepository.save(createAnime());
+        Assertions.assertThat(savedAnime).isNotNull();
+        Assertions.assertThat(savedAnime.getId()).isNotNull();
+        Assertions.assertThat(savedAnime.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    private Anime createAnime() {
+        return Anime.builder()
+                .name("Hajime no Ippo")
+                .build();
+    }
+}
