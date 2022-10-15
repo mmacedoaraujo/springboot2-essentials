58c71a32aaa87cea1abddedb046daf86dd5aa037
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-15 11:27:58 -0300

    spring security pt01 - xsrf token

--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @Log4j2
@@ -14,7 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable()
+                .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
