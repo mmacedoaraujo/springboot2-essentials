301b7c13e7a1f4aca2c99cc8c6e600be0ee69c72
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-21 20:56:25 -0300

    spring security pt 03 - authentication principal and default login page

--- springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/config/SecurityConfig.java
+++ springboot2-essentials/src/main/java/com/mmacedo/springboot2essentials/config/SecurityConfig.java
@@ -14,13 +14,24 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @Log4j2
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    /**
+     * BasicAuthenticationFilter
+     * UsernamePasswordAuthenticationFilter
+     * DefaultLoginPageGeneratingFilter
+     * DefaultLogoutPageGeneratingFilter
+     * FilterSecurityInterceptor
+     * Authentication -> Authorization
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .formLogin()
                 .and()
                 .httpBasic();
     }
